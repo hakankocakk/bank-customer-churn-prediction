245f5e3cd034648c18a44e39e4da0961e82a4b9b
Hakan Koçak <hakan.kocak1@std.yildiz.edu.tr>
2024-12-30 12:55:10 +0300

    update main.py : for all steps preprocess.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import preprocess
 import joblib
+from imblearn.under_sampling import NearMiss
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.compose import ColumnTransformer
@@ -8,45 +9,26 @@ from sklearn.preprocessing import FunctionTransformer, StandardScaler
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, f1_score
 
+
 def main():
 
     dataframe = pd.read_csv("C:/Users/Hakan/bank-customer-churn-prediction/data/train.csv")
     X = dataframe.drop(["id", "CustomerId", "Surname", "Exited"], axis=1)
     y = dataframe["Exited"]
+
     X_train, X_, y_train, y_ = train_test_split(X, y, test_size=0.3, random_state=42)
     X_val, X_test, y_val, y_test = train_test_split(X_, y_, test_size=0.15, random_state=42)
 
-    preprocessing = preprocess.Preprocess(X_train)
-    preprocessing.create_col_type()
-
-    numeric_processor = Pipeline(
-        steps=[
-            ("impute_missing", FunctionTransformer(preprocessing.Impute_missing_num_data)),
-            ("impute_outlier", FunctionTransformer(preprocessing.Impute_outlier_data)),
-            #("feature_engineering", FunctionTransformer(lambda X: preprocessing.feature_engineering_num(X)))
-        ]
-    )
-
-    categoric_processor = Pipeline(
-        steps=[
-            ("impute_missing", FunctionTransformer(preprocessing.Impute_missing_cat_data)),
-            #("feature_engineering", FunctionTransformer(lambda X: preprocessing.feature_engineering_cat(X))),
-            #("ordinal_encoding", FunctionTransformer(lambda X: preprocessing.ordinalencoding(X))),
-            ("onehot_encoding", FunctionTransformer(preprocessing.onehotencoding))
-        ]
-    )
-
-
-    preprocessor_pipeline = ColumnTransformer(
-        transformers=[
-            ("numerical", numeric_processor, preprocessing.num_cols),
-            ("categorical", categoric_processor, preprocessing.cat_cols)
-        ]
-    )
+    preprocessing = preprocess.Preprocess(X_train, y_train)
 
     pipeline = Pipeline(
         steps=[
-            ("preprocessor", preprocessor_pipeline),
+            ("impute_missing_num", FunctionTransformer(preprocessing.Impute_missing_num_data)),
+            ("impute_missing_cat", FunctionTransformer(preprocessing.Impute_missing_cat_data)),
+            ("impute_outlier", FunctionTransformer(preprocessing.Impute_outlier_data)),
+            ("feature_engineering", FunctionTransformer(preprocessing.feature_engineering)),
+            ("ordinal_encoding", FunctionTransformer(preprocessing.ordinalencoding_num)),
+            ("onehot_encoding", FunctionTransformer(preprocessing.onehotencoding)),
             ("scaler", StandardScaler()), 
             ("model", XGBClassifier())
         ]
