0d265388805b8a39b1ac7883efc85267c1947e35
Hakan Koçak <hakan.kocak1@std.yildiz.edu.tr>
2025-02-14 22:51:41 +0300

    update inference.py

--- src/Model/inference.py
+++ src/Model/inference.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 from ..Preprocess.preprocess import Preprocess
 from sklearn.ensemble import VotingClassifier
 import joblib
@@ -7,21 +8,9 @@ import click
 
 
 def load_model():
-    xgboost_model = joblib.load("models/xgboost_model.pkl")
-    lightgbm_model = joblib.load("modelslightgbm_model.pkl")
-    catboost_model = joblib.load("models/catboost_model.pkl")
 
-    ensemble_model = VotingClassifier(
-        estimators=[
-            ("XgBoost", xgboost_model),
-            ("LightGBM", lightgbm_model),
-            ("CatBoost", catboost_model)
-        ],
-        voting="soft"
-    )
-
-    joblib.dump(ensemble_model, 'C:/Users/Hakan/Documents/GitHub/bank-customer-churn-prediction/src/Model/models/ensemble_model.pkl')
-    ensemble_model = joblib.load("C:/Users/Hakan/Documents/GitHub/bank-customer-churn-prediction/src/Model/models/ensemble_model.pkl")
+    model_path = os.path.join(os.path.join(os.path.dirname(__file__), "models"))
+    ensemble_model = joblib.load(os.path.join(model_path, "ensemble_model.pkl"))
 
     return ensemble_model
 
@@ -30,11 +19,13 @@ def load_model():
 def predict(data_path):
     dataframe = pd.read_csv(data_path)
     preprocess = Preprocess(dataframe)
+
     preprocess_data = preprocess.preprocess_pipeline()
     ensemble_model = load_model()
     predict = ensemble_model.predict(preprocess_data)
+    
     print(predict)
-
+ 
 
 if __name__ == '__main__':
     predict()
